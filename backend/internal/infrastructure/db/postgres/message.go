eee7a01eb607c8bf1980339c4ed0fdf211c81732
codebytecs <va.pogoreloff@gmail.com>
2025-07-23 16:37:19 +0700

    make message's functions for db

--- backend/internal/infrastructure/db/postgres/message.go
+++ backend/internal/infrastructure/db/postgres/message.go
@@ -1,1 +1,28 @@
 package postgres
+
+import (
+	"GoChat/internal/domain"
+	"github.com/jmoiron/sqlx"
+)
+
+func SaveMessage(db *sqlx.DB, msg domain.Message) error {
+	_, err := db.Exec("INSERT INTO messages (sender, receiver, message) VALUES ($1, $2, $3), msg.Sender, msg.Receiver, msg.Text)")
+	return err
+}
+
+func GetMessagesBetweenUsers(db *sqlx.DB, user1 string, user2 string) ([]domain.Message, error) {
+	var messages []domain.Message
+	err := db.Select(&messages, "SELECT * FROM messages "+
+		"WHERE (sender = $1 AND receiver = $2) OR (sender = $2 AND receiver = $1) "+
+		"ORDER BY created_at", user1, user2)
+	return messages, err
+}
+
+func GetLastMessagesForUser(db *sqlx.DB, u domain.User) ([]domain.Message, error) {
+	var messages []domain.Message
+	err := db.Select(&messages, "SELECT * FROM messages "+
+		"WHERE receiver = $1"+
+		"ORDER BY created_at DESC"+
+		"LIMIT 20", u.Username)
+	return messages, err
+}
--- backend/internal/repository/message.go
+++ backend/internal/repository/message.go
@@ -1,4 +1,12 @@
 package repository
 
+import (
+	"GoChat/internal/domain"
+	"github.com/jmoiron/sqlx"
+)
+
 type MessageRepository interface {
+	SaveMessage(db *sqlx.DB, msg domain.Message) error
+	GetMessagesBetweenUsers(db *sqlx.DB, user1 string, user2 string) ([]domain.Message, error)
+	GetLastMessagesForUser(db *sqlx.DB, u domain.User) ([]domain.Message, error)
 }
